de3b99a56c5a5bfa27d7893d1aa9478365b39444
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2021-06-05 16:22:54 +0900

    init

--- calc/ContentView.swift
+++ calc/ContentView.swift
@@ -8,9 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var adultStr = ""
+    @State var childStr = ""
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            Text("料金は \(calc()) 円です。")
+            HStack{
+                Text("大人")
+                TextField("0", text: $adultStr)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .keyboardType(.numberPad)
+            }
+            HStack{
+                Text("子供")
+                TextField("0", text: $childStr)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .keyboardType(.numberPad)
+            }
+        }
+    }
+    
+    // Int? 型
+    // → Int 型 もしくは nil
+    func calc () -> Int {
+        var adult = 0, child = 0
+        if let a = Int(adultStr) {
+            adult = a
+        }
+        if let c = Int(childStr) {
+            child = c
+        }
+//        guard let adult = Int(adultStr) else {
+//            return 0
+//        }
+//        guard let child = Int(childStr) else {
+//            return 0
+//        }
+        
+        let money = adult * 1200 + child * 500
+        return money
     }
 }
 
