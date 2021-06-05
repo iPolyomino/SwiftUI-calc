1cd793ba4e9f68e2d22a2fb34d9849304659903f
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2021-06-05 16:07:41 +0900

    Initial Commit

---
+++ calc/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  calc
+//
+//  Created by 萩倉丈 on 2021/06/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ calc/calcApp.swift
@@ -0,0 +1,17 @@
+//
+//  calcApp.swift
+//  calc
+//
+//  Created by 萩倉丈 on 2021/06/05.
+//
+
+import SwiftUI
+
+@main
+struct calcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
